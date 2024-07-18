1d038284abbeb6fa2d8c021dabf683f918ce5182
Pavel.Sergeev <pavel.sergeev@jetbrains.com>
2024-07-18 21:34:28 +0200

    depth buffer

--- src/state.rs
+++ src/state.rs
@@ -7,7 +7,7 @@ use winit::{
     window::Window,
 };
 
-use crate::{texture, camera::{CameraModel, CameraUniform, CameraController, CameraState}};
+use crate::{texture::{self, Texture}, camera::{CameraModel, CameraUniform, CameraController, CameraState}};
 use crate::mesh::{Mesh, Vertex};
 use crate::instances::{Instances, Rotation};
 
@@ -25,6 +25,7 @@ pub struct State {
     camera: CameraState,
     rotator: Rotation,
     pub instances: Instances,
+    depth_texture: Texture,
 }
 
 impl State {
@@ -138,6 +139,8 @@ impl State {
             }
         );
 
+        let depth_texture = texture::Texture::create_depth_texture(&device, &config, "depth_texture");
+
         let mesh = Mesh::new(&device);
 
         let camera_bind_group_layout = CameraState::layout(&device);
@@ -166,6 +169,7 @@ impl State {
             rotator,
             instances,
             diffuse_bind_group,
+            depth_texture,
         }
     }
 
@@ -213,7 +217,13 @@ impl State {
                 // Requires Features::CONSERVATIVE_RASTERIZATION
                 conservative: false,
             },
-            depth_stencil: None, // 1.
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less, // 1.
+                stencil: wgpu::StencilState::default(), // 2.
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1,                         // 2.
                 mask: !0,                         // 3.
@@ -228,11 +238,12 @@ impl State {
     }
 
     pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        if new_size.width > 0 && new_size.height > 0 {
+        if new_size.width > 0 && new_size.height > 0 && new_size.width <= 8192 && new_size.height <= 8192 {
             self.size = new_size;
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+            self.depth_texture = Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
         }
     }
 
@@ -275,7 +286,14 @@ impl State {
                         store: true,
                     },
                 })],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: true,
+                    }),
+                    stencil_ops: None,
+                }),
             });
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
